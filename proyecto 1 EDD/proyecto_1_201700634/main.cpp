f00d9841dc3e74910eda47c426eaa039db0e35a3
cesarchs <58505340+cesarchs@users.noreply.github.com>
2020-09-20 21:34:28 -0600

    parte 1 proyecto edd

---
+++ proyecto 1 EDD/proyecto_1_201700634/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*****************************************************"<<endl;
+    cout << "* USAC                                              *"<<endl;
+    cout << "*ESTRUCTURA DE DATOS                                *"<<endl;
+    cout << "*SECCION B                                          *"<<endl;
+    cout << "*CESAR LEONEL CHAMALE SICAN                         *"<<endl;
+    cout << "*201700634                                          *"<<endl;
+    cout << "*****************************************************"<<endl<<endl;
+    cout << "*    INTRODUCA EL INDICE DE LO QUE DESEA HACER      *"<<endl<<endl;
+    cout << "* 1. VER PROYECTOS                                  *"<<endl;
+    cout << "* 2. EDITAR PROYECTOS                               *"<<endl;
+    cout << "* 3. CARGAR PROYECTO                                *"<<endl;
+    cout << "* 4. GRAFICAR PROYECTOS                             *"<<endl;
+    cout << "* 5. GUARDAR PROYECTOS                              *"<<endl;
+    cout << "* 6. CARGAR LIBRERIAS                               *"<<endl;
+    cout << "*****************************************************"<<endl;
+    return 0;
+}
+
